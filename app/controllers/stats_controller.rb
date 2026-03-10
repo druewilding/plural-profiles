f7db2751da7a78b3cef50287e56b1b4dff59e21b
Dru Wilding <drue+github@wilding.dk>
2026-03-10 19:57:06 +0100

    feat: Add theme count to stats page (#106)

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,5 +7,6 @@ class StatsController < ApplicationController
     @group_count = Group.count
     @avatar_count = ActiveStorage::Attachment.where(name: "avatar", record_type: [ "Profile", "Group" ]).count
     @invite_code_count = InviteCode.where(redeemed_at: nil).count
+    @theme_count = Theme.count
   end
 end
