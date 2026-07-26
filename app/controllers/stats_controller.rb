c64a5f3f79331e167de0860357aa5524ef5f9d24
druwilding <drue+github@wilding.dk>
2026-07-26 10:29:49 +0200

    feat: Add chat-related stats to the stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -8,5 +8,10 @@ class StatsController < ApplicationController
     @avatar_count = ActiveStorage::Attachment.where(name: "avatar", record_type: [ "Profile", "Group" ]).count
     @invite_code_count = InviteCode.where(redeemed_at: nil).count
     @theme_count = Theme.count
+    @chat_server_count = Chat::Server.count
+    @chat_channel_count = Chat::Channel.count
+    @chat_message_count = Chat::Message.count
+    @chat_membership_count = Chat::Membership.count
+    @chat_server_invite_count = Chat::ServerInvite.unredeemed.count
   end
 end
